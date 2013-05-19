90e8b412ff6cfd80a7184c8371d95c7090123787
Scyntrus <Scyntrus@gmail.com>
2013-05-19 16:57:51 -0700

    Reverted to old entity tracking method
    To fix bug with Titans unable to be melee attacked.

--- src/com/gmail/scyntrus/fmob/ChunkMobLoader.java
+++ src/com/gmail/scyntrus/fmob/ChunkMobLoader.java
@@ -1,5 +1,9 @@
 package com.gmail.scyntrus.fmob;
 
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
+
+import net.minecraft.server.v1_5_R3.WorldServer;
+
 public class ChunkMobLoader implements Runnable {
 
 	FactionMobs plugin;
@@ -12,8 +16,14 @@ public class ChunkMobLoader implements Runnable {
 		if (FactionMobs.scheduleChunkMobLoad) {
 			FactionMobs.scheduleChunkMobLoad = false;
 			for (FactionMob fmob : FactionMobs.mobList) {
-				Utils.addEntity(fmob.getEntity().world, fmob.getEntity()); //TODO: Fix problem
-				fmob.getEntity().dead = false;
+//				Utils.addEntity(fmob.getEntity().world, fmob.getEntity()); //TODO: Fix problem
+//				fmob.getEntity().dead = false;
+	 			if (!((WorldServer) fmob.getEntity().world).getTracker().trackedEntities.b(fmob.getEntity().id)) {
+					try	{
+						fmob.getEntity().world.addEntity(fmob.getEntity(), SpawnReason.CUSTOM);
+					} catch (Exception ex) {}
+					fmob.getEntity().dead = false;
+	 			}
 			}
 		}
     }
