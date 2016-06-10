8ab95fd9feac1f28a9550387570de229223f640b
Scyntrus <Scyntrus@gmail.com>
2016-06-10 00:50:03 -0700

    Avoid call to Math.pow for performance

--- src/com/gmail/scyntrus/fmob/Utils.java
+++ src/com/gmail/scyntrus/fmob/Utils.java
@@ -129,7 +129,10 @@ public class Utils {
     }
 
     public static double dist3D(double x1, double x2, double y1, double y2, double z1, double z2) {
-        return Math.sqrt(Math.pow(x1-x2,2) + Math.pow(y1-y2,2) + Math.pow(z1-z2,2));
+        double dx = x1 - x2;
+        double dy = y1 - y2;
+        double dz = z1 - z2;
+        return Math.sqrt(dx * dx + dy * dy + dz * dz);
     }
 
     public static double countMobPowerInFaction(Faction faction) {
